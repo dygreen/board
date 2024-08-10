8564dbc9147e88191597497332494acaf21a728b
dygreen <ehdus9877@naver.com>
2024-08-10 16:11:11 +0900

    #refactor : 회원가입 API 타입, 변수명 수정

--- src/app/api/auth/signUp/route.ts
+++ src/app/api/auth/signUp/route.ts
@@ -1,18 +1,18 @@
 import { connectDB } from '@util/database'
 import bcrypt from 'bcrypt'
-import { NextResponse } from 'next/server'
+import { NextRequest, NextResponse } from 'next/server'
 
-async function signUp(req: Request, res: Response) {
+async function signUp(req: NextRequest, res: NextResponse) {
     const db = (await connectDB).db('board')
 
     try {
         // Next.js App Router에서는 req.json()으로 요청 본문을 가져옵니다.
-        const res = await req.json()
+        const body = await req.json()
 
         // 중복된 이메일 체크 (유저가 보낸 이메일이 db에 있으면 회원가입 시켜주지 않게)
         let dupliUser = await db
             .collection('user_cred')
-            .findOne({ email: res.email })
+            .findOne({ email: body.email })
 
         if (dupliUser) {
             return NextResponse.json(
@@ -22,11 +22,11 @@ async function signUp(req: Request, res: Response) {
         }
 
         // bcrypt : 비번 암호화 저장
-        res.password = await bcrypt.hash(res.password, 10)
-        res.role = 'normal'
+        body.password = await bcrypt.hash(body.password, 10)
+        body.role = 'normal'
 
         // 회원정보 보관
-        await db.collection('user_cred').insertOne(res)
+        await db.collection('user_cred').insertOne(body)
 
         return NextResponse.json({ message: '회원 가입 되었습니다.' })
     } catch (error) {
@@ -37,11 +37,10 @@ async function signUp(req: Request, res: Response) {
     }
 }
 
-export async function POST(req: Request, res: Response) {
+export async function POST(req: NextRequest, res: NextResponse) {
     try {
         return await signUp(req, res)
     } catch (e) {
-        console.error(e)
         return NextResponse.json(
             { message: 'Internal Server Error' },
             { status: 500 },
