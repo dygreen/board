3975f7ad917268208c64f24edf278247c62b02ae
dygreen <ehdus9877@naver.com>
2024-08-11 18:00:14 +0900

    #fix : env 변수명 수정

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,10 +5,14 @@ export default async function Home() {
     // 게시글 리스트 조회
     const getData = async () => {
         try {
-            const res = await fetch('/api/board/get-article', {
-                cache: 'no-store',
-            })
-            return await res.json()
+            const res = await fetch(
+                `${process.env.BASE_URL}/api/board/get-article`,
+                {
+                    cache: 'no-store',
+                },
+            )
+            const data = await res.json()
+            return data
         } catch (error) {
             console.error(error)
         }
--- util/database.ts
+++ util/database.ts
@@ -1,21 +1,23 @@
 import { MongoClient } from 'mongodb'
 
 declare global {
-    var _mongo: Promise<MongoClient>
+    var _mongo: Promise<MongoClient> | undefined
 }
 
-const options = { useNewUrlParser: true }
+const url = process.env.MONGODB_URL
+if (!url) {
+    throw new Error('The MONGODB_URL environment variable is not defined')
+}
+
 let connectDB: Promise<MongoClient>
 
 if (process.env.NODE_ENV === 'development') {
     if (!global._mongo) {
-        global._mongo = new MongoClient(
-            process.env.NEXT_PUBLIC_URL as string,
-        ).connect()
+        global._mongo = new MongoClient(url).connect()
     }
     connectDB = global._mongo
 } else {
-    connectDB = new MongoClient(process.env.NEXT_PUBLIC_URL as string).connect()
+    connectDB = new MongoClient(url).connect()
 }
 
 export { connectDB }
