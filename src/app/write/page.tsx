b700bb0ad2baf861b8cd73398067713a9fcb4a2d
dygreen <ehdus9877@naver.com>
2024-08-23 16:41:45 +0900

    #feat : 로그인 상태일 경우에만 게시글 작성할 수 있도록 세팅

--- src/app/write/page.tsx
+++ src/app/write/page.tsx
@@ -1,7 +1,14 @@
-'use client'
-
 import FormArea from '@components/board/FormArea'
+import { getServerSession } from 'next-auth'
+import { authOptions } from '@src/app/api/auth/[...nextauth]/route'
+import { redirect } from 'next/navigation'
 
-export default function Write() {
+export default async function Write() {
+    const session = await getServerSession(authOptions)
+
+    if (!session) {
+        redirect('/api/auth/signin')
+    }
+
     return <FormArea isModify={false} />
 }
