462144a4c18bc0e68b18d9a19f892ec085a6d06f
dygreen <ehdus9877@naver.com>
2024-08-25 16:20:51 +0900

    #fix : 경로 build 에러 수정 방식 변경

--- src/app/loading.js
+++ src/app/loading.js
@@ -1,11 +1,15 @@
 import React from 'react'
 import Image from 'next/image'
-import spinner from '@public/images/spinner.gif'
 
 export default function Loading() {
     return (
         <div className="loading-container">
-            <Image src={spinner} alt="로딩중" />
+            <Image
+                src="/images/spinner.gif"
+                alt="로딩중"
+                width={100}
+                height={100}
+            />
             <div className="loading-text">
                 <h4>처리 중입니다.</h4>
                 <span>잠시만 기다려주세요.</span>
