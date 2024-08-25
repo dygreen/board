aea60f9b06264105cee4d824c12b7831ccb8cec9
dygreen <ehdus9877@naver.com>
2024-08-25 14:45:13 +0900

    #feat : home 버튼 추가

--- layout/Footer/Footer.tsx
+++ layout/Footer/Footer.tsx
@@ -1,13 +1,23 @@
-import { faPlus } from '@fortawesome/free-solid-svg-icons'
+'use client'
+
+import { faHouse, faPlus } from '@fortawesome/free-solid-svg-icons'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import styles from './footer.module.scss'
+import styles from '@layout/Footer/footer.module.scss'
 import Link from 'next/link'
+import { usePathname } from 'next/navigation'
 
 export default function Footer() {
+    const pathname = usePathname()
+
     return (
         <footer className={styles.footer}>
             <div className={styles.container}>
-                <Link href={'/write'}>
+                {pathname !== '/' && (
+                    <Link href={'/'} className={styles.homeBtn}>
+                        <FontAwesomeIcon icon={faHouse} />
+                    </Link>
+                )}
+                <Link href={'/write'} className={styles.writeBtn}>
                     <FontAwesomeIcon icon={faPlus} />
                 </Link>
             </div>
